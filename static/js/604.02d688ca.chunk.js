68b9b0e55f2293185192ae2d2f1fa7cdfec2a3c8
Sebastian Barbosa <sebabarbosa@gmail.com>
2023-04-27 19:14:23 -0300

    Updates

---
+++ static/js/604.02d688ca.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkffx_monster_arena=self.webpackChunkffx_monster_arena||[]).push([[604],{604:function(n,e,s){s.r(e);var a=s(184);e.default=function(){return(0,a.jsx)("section",{className:"section is-main-section",children:"Page not found"})}}}]);
+//# sourceMappingURL=604.02d688ca.chunk.js.map
